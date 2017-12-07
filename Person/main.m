6a6797e6b89bd9c4b1f3857754e47d3b96352a0c
mengjiu999 <1099399203@qq.com>
2017-12-07 13:15:33 +0800

    Initial Commit

---
+++ Person/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Person
+//
+//  Created by apple on 2017/12/7.
+//  Copyright © 2017年 YuNuo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
